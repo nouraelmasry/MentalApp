9923d4d7ee14420396e1c273db375eb71ae69257
Nour <future@DESKTOP-2192630>
2024-05-26 01:33:12 +0300

    ChatBot Screen

--- Navigations/TabNavigation.jsx
+++ Navigations/TabNavigation.jsx
@@ -5,46 +5,58 @@ import HomePage from '../Screens/HomeScreen/HomePage';
 import BookingPage from '../Screens/BookingScreen/BookingPage'
 import ProfilePage from '../Screens/ProfileScreen/ProfilePage';
 import { FontAwesome } from '@expo/vector-icons';
+import ChatScreen from '../Screens/ChatBot/ChatScreen';
+import { Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
-  return (
-    <Tab.Navigator screenOptions={{
-        headerShown:false,
-        tabBarActiveTintColor:"#9bb169"
+    return (
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#9bb169"
         }}>
-        <Tab.Screen name='home' component={HomePage}
-        options={{
-            tabBarLabel:({color})=>(
-                <Text style={{color:color, fontSize:12, marginTop:-7, marginBottom:5}}>Home</Text>
-            ),
-            tabBarIcon:({color, size})=>(
-                <FontAwesome name="home" size={24} color={color} />
-            )
-        }}
-        />
-        <Tab.Screen name='booking' component={BookingPage}
-        options={{
-            tabBarLabel:({color})=>(
-                <Text style={{color:color, fontSize:12, marginTop:-7, marginBottom:5}}>Booking</Text>
-            ),
-            tabBarIcon:({color, size})=>(
-                <FontAwesome name="bookmark" size={24} color={color} />
-            )
-        }}
-        />
-        <Tab.Screen name='profile' component={ProfilePage}
-        options={{
-            tabBarLabel:({color})=>(
-                <Text style={{color:color, fontSize:12, marginTop:-7, marginBottom:5}}>Profile</Text>
-            ),
-            tabBarIcon:({color, size})=>(
-                <FontAwesome name="user" size={24} color={color} />
-            )
-        }}
-        />
-    </Tab.Navigator>
-  )
+            <Tab.Screen name='home' component={HomePage}
+                options={{
+                    tabBarLabel: ({ color }) => (
+                        <Text style={{ color: color, fontSize: 12, marginTop: -7, marginBottom: 5 }}>Home</Text>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="home" size={24} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen name='booking' component={BookingPage}
+                options={{
+                    tabBarLabel: ({ color }) => (
+                        <Text style={{ color: color, fontSize: 12, marginTop: -7, marginBottom: 5 }}>Booking</Text>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="bookmark" size={24} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen name='Ask Ai' component={ChatScreen}
+                options={{
+                    tabBarLabel: ({ color }) => (
+                        <Text style={{ color: color, fontSize: 12, marginTop: -7, marginBottom: 5 }}>Ask Ai</Text>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <Entypo name="chat" size={24} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen name='profile' component={ProfilePage}
+                options={{
+                    tabBarLabel: ({ color }) => (
+                        <Text style={{ color: color, fontSize: 12, marginTop: -7, marginBottom: 5 }}>Profile</Text>
+                    ),
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="user" size={24} color={color} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
 }
 
 export default TabNavigation
